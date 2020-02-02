45fa4dbce02931b68931211f56f71cb463ee33e9
Dharmapala Varappade <dvarappade@yodlee.com>
2020-02-02 21:02:34 +0530

    login page

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 import "./App.scss";
@@ -14,8 +15,15 @@ class HomeApp extends Component {
 
   constructor(props) {
     super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.showRegister = this.showRegister.bind(this);
+    this.showLogin = this.showLogin.bind(this);
+
     this.state = {
-      isLogginActive: true
+      isLogginActive: true,
+      username:'',
+      password:'',
+      isNewUser:false
     };
   }
 
@@ -23,11 +31,94 @@ class HomeApp extends Component {
     this.props.action(true);
   }
 
+  showRegister(){
+    this.setState({isNewUser:true})
+  }
+  showLogin(){
+    this.setState({isNewUser:false})
+  }
+  onSubmit(e) {
+    e.preventDefault();
+ 
+     const obj = {
+       username: this.state.username,
+       password : this.state.password
+     };
+ 
+     this.props.action(true);
+ /*
+     axios.post('http://localhost:4000/department/add', obj)
+         .then(res => console.log(res.data));
+         //this.props.history.push('/department');
+         this.props.action();
+         
+     this.setState({
+       username: '',
+       password: ''
+     }) */
+   }
 
   render() {
-
-
-
+    const isNewUser = this.state.isNewUser;
+    console.log("isNewUser:"+isNewUser);
+    let body;
+    if(!isNewUser) {
+        body = <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                      <label>User name:  </label>
+                      <input 
+                        type="text" 
+                        className="form-control" 
+                        value={this.state.Department_name}
+                        onChange={this.onChangeDepartmentName}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Password: </label>
+                      <input type="text" 
+                        className="form-control"
+                        value={this.state.Department_id}
+                        onChange={this.onChangeDepartmentId}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <input type="submit" 
+                      value="Login" 
+                        className="btn btn-primary"/>
+                  </div>
+                  <div>
+                    <Link  onClick={ e => this.showRegister()}  >Click here to register</Link>
+                  </div>
+              </form>
+        } else {
+          body = <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+              <label>Employee Code:  </label>
+              <input 
+                type="text" 
+                className="form-control" 
+                value={this.state.employeeCode}
+                onChange={this.onChangeDepartmentName}
+                />
+          </div>
+          <div className="form-group">
+              <label>Password: </label>
+              <input type="text" 
+                className="form-control"
+                value={this.state.password}
+                onChange={this.onChangeDepartmentId}
+                />
+          </div>
+          <div className="form-group">
+              <input type="submit" 
+              value="Register" 
+                className="btn btn-primary"/>
+          </div>
+          <div>
+            <Link  onClick={ e => this.showLogin()}  >Click here to login</Link>
+          </div>
+      </form>
+        }
 
     return (
 
@@ -35,11 +126,10 @@ class HomeApp extends Component {
   <div className="App">
         <div className="login">
           <div className="container" ref={ref => (this.container = ref)}>
-          <Link onClick={ e => this.onLogin()}>Click here to login</Link>
+            {body}
 
           </div>
-
-        </div>
+         </div>
       </div>
       
 
